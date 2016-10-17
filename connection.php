25fb998c4b33141f3320298b6ab595ce6d2e4862
laurene <laurene.yviquel@epfprojets-sceaux.com>
2016-10-17 23:51:06 +0200

    premier site concours

---
+++ connection.php
@@ -0,0 +1,130 @@
+<?php include("include.php"); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Connexion</title>
+        <meta charset="utf-8" />
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+        <link href="bootstrap/css/tuto.css" rel="stylesheet">
+        <style type="text/css">
+            .col-lg-2 { line-height: 100px; }
+            .col-lg-5 { line-height: 200px; }
+            .col-lg-12 { line-height: 80px; }
+          body { background-color:#DDD; }
+          [class*="col"] { margin-bottom: 20px; }
+          img { width: 100%; }
+          .well {
+            background-color:#CCC;
+            padding: 20px;
+          }
+            @media (min-width: 768px) {
+          .container {
+            width: 750px;
+          }
+          }
+          @media (min-width: 992px) {
+            .container {
+              width: 970px;
+            }
+          }
+          @media (min-width: 1200px) {
+            .container {
+              width: 1170px;
+            }
+          }
+          </style>
+    </head>
+    <body id="connection">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+            <h1>Connexion</h1>
+        </div>
+        <div class="col-sm-12">
+        <p>Veuillez entrer le mot de passe pour obtenir le droit d'inscrire, de modifier et de supprimer les résultats en concours des cavaliers et de leur monture :</p>
+
+        </div>
+        <div class="col-sm-12">
+        <?php
+        // Le mot de passe n'a pas été envoyé ou n'est pas bon
+        if (!isset($_POST['mot_de_passe']) OR $_POST['mot_de_passe'] != "admin")
+        {
+            ?>
+            <!-- Afficher le formulaire de saisie du mot de passe -->
+        <form action="connection.php" method="post" class="form-inline">
+            <div class="input-group col-lg-4">
+                <input type="password" name="mot_de_passe" class="form-control">
+                <?php
+                $_SESSION['prenom'] = 'mot_de_passe';
+                ?>
+                <span class="input-group-btn">
+                  <button class="btn btn-default" type="submit">Valider</button>
+                </span>
+            </div>
+        </form>
+        <?php
+        }
+        // Le mot de passe a été envoyé et il est bon
+        else
+        {
+            ?>
+            <!-- Afficher les codes secrets-->
+            <div class="col-sm-12">Vous etes connecté </div>
+            <?php
+            $_SESSION['prenom'] = 'autorise';
+            ?>
+            <?php
+        }
+        ?>
+
+<!--
+        <div class="col-lg-4">
+          <form class="form-inline well">
+            <div class="form-group">
+              <label class="sr-only" for="text">Saisie</label>
+              <input id="text" type="password" name="mot_de_passe" class="form-control" placeholder="Mot de passe ici">
+              <?php
+               // $_SESSION['prenom'] = 'mot_de_passe';
+                ?>
+            </div>
+            <button type="submit" class="btn btn-primary pull-right"><span class = "glyphicon glyphicon-user" ></span> Connexion</button>
+            <div class="alert alert-block alert-danger" style="display:none">
+              <h4>Erreur !</h4>
+              Votre mot de passe n'est pas valide ! 
+            </div>
+          </form>
+        </div>
+        <script src="bootstrap/js/jquery-3.1.1.js"></script> 
+        <script>
+          $(function(){
+            $("form").on("submit", function() {
+              if(!isset($_POST['mot_de_passe']) OR $_POST['mot_de_passe'] != "admin") {
+                $("div.form-group").addClass("has-error");
+                $("div.alert").show("slow").delay(4000).hide("slow");
+                return false; 
+              }
+               // Le mot de passe a été envoyé et il est bon
+                else
+                {
+                    
+                    //Vous etes connecté 
+                    <?php
+                   // $_SESSION['prenom'] = 'autorise';
+                    ?>
+                    
+                }
+
+            });
+          });
+        </script>
+-->
+        </div>
+         <div class="col-sm-12">
+        <p>Si tu veux lire la liste des cavaliers déjà inscrit, <a href="site03.php">clique ici</a> pour revenir à la page précédente site.php.</p>
+        </div>
+      </div>
+      </div>
+    
+
+    </body>
+</html>
---
+++ pied_de_page.php
@@ -0,0 +1,42 @@
+<footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-4">
+            <h1>Derniers commentaires</h1>
+            <p>Merci beaucoup, super site !!!</p>
+            <p>le 12/09/16 à 23h12</p>
+        </div>
+        <div class="col-sm-4">
+            <h1>Droit</h1>
+            <p>Copyright moi, tous droits réservés</p>
+            <button data-toggle="modal" href="#infos" class="btn btn-primary">Informations</button>
+            <div class="modal" id="infos">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">x</button>
+                    <h4 class="modal-title">Plus d'informations : Appliqué au site Web</h4>
+                  </div>
+                  <div class="modal-body">
+                    Appliqué au site Web
+Dans un site Web, il est possible de protéger plusieurs éléments sous différentes catégories. Par exemple, le code (HTML, JavaScript, etc.) et le texte seront protégés comme œuvres littéraires alors que le design et les images seront considérés comme des œuvres artistiques, etc. Bien qu’il ne soit pas obligatoire d’apposer la mention en pied de page pour être protégé au Canada, il est tout de même suggéré de le faire. Cela permet de rappeler aux gens que le site est assujetti à la loi. De plus, dans certains pays, on ne reconnaîtra le droit d’auteur que si cette mention apparaît au bas d’un site Web (OPIC 2010).
+                  </div>
+                  <div class="modal-footer">
+                    <button class="btn btn-info" data-dismiss="modal">Fermer</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+        <div class="col-sm-4">
+            <h1>Les liens</h1>
+            <div id="listes_amis">
+                <ul class="list-unstyled">
+                    <li><a href="http://www.ffe.com/">Site FFE</a></li>
+                    <li><a href="https://ffecompet.ffe.com/concours">Liste compétitions FFE</a></li>
+                </ul>
+            </div>
+        </div>
+      </div>
+    </div>
+</footer>
