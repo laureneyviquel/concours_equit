67240fb64fd541f1815ece464b436f78b00a5f4e
laurene <laurene.yviquel@epfprojets-sceaux.com>
2016-10-17 23:57:44 +0200

    envoyer site concours 2

--- banniere.php
+++
@@ -1,5 +0,0 @@
-			<div id="banniere_image">
-                <div id="banniere_description">
-                    Bienvenue sur le site pour suivre les résultats des concours d'équitation !
-                </div>
-            </div>
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php include("include.php"); ?>
-<html>
-<?php
-$people = User::order_by_desc('note')->find_many();
-                foreach ($people as $one) {
-                  echo $one->cheval;
-                }
-                ?>
-                </html>
